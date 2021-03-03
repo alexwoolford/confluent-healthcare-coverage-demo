ec74aed6e8fdd727db3f1707c858bc627dcb3f69
Alex Woolford <alex@woolford.io>
2021-03-02 22:51:40 -0700

    create single partition ailment topic

--- src/main/java/io/woolford/demo/KafkaConfig.java
+++ src/main/java/io/woolford/demo/KafkaConfig.java
@@ -1,11 +1,13 @@
 package io.woolford.demo;
 
 import io.woolford.demo.database.entity.AilmentRecord;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -37,6 +39,14 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, AilmentRecord> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic createIrisTopic() {
+        return TopicBuilder.name("ailment")
+                .partitions(1)
+                .replicas(3)
+                .build();
     }
 
 }
